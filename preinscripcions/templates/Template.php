eb4277315738d137b03d6a4d478a03e111868f0b
Pedro <enfokat@gmail.com>
2016-11-17 09:00:30 +0100

    v1 config/config.php -> configurado

---
+++ preinscripcions/templates/Template.php
@@ -0,0 +1,79 @@
+<?php
+	class Template{	
+		
+		//PONE EL HEADER DE LA PAGINA
+		public static function header(){	?>
+			<header>
+				<figure>
+					<a href="index.php">
+						<img alt="Robs Micro Framework logo" src="images/logos/logo.png" />
+					</a>
+				</figure>
+				<hgroup>
+					<h1>Pre-Inscripcions</h1>
+					<h2>Apuntate a los cursos que te interese</h2>
+				</hgroup>
+			</header>
+		<?php }
+		
+		
+		//PONE EL FORMULARIO DE LOGIN
+		public static function login(){?>
+			<form method="post" id="login" autocomplete="off">
+				<label>User:</label><input type="text" name="user" required="required" />
+				<label>Password:</label><input type="password" name="password" required="required"/>
+				<input type="submit" name="login" value="Login" />
+			</form>
+		<?php }
+		
+		
+		//PONE LA INFO DEL USUARIO IDENTIFICADO Y EL FORMULARIOD E LOGOUT
+		public static function logout($usuario){	?>
+			<div id="logout">
+				<span>
+					Hola 
+					<a href="index.php?controlador=Usuario&operacion=modificacion" title="modificar datos">
+						<?php echo $usuario->nombre;?></a>
+					<span class="mini">
+						<?php echo ' ('.$usuario->email.')';?>
+					</span>
+					<?php if($usuario->admin) echo ', eres administrador';?>
+				</span>
+								
+				<form method="post">
+					<input type="submit" name="logout" value="Logout" />
+				</form>
+				
+				<div class="clear"></div>
+			</div>
+		<?php }
+		
+		
+		//PONE EL MENU DE LA PAGINA
+		public static function menu($usuario){ ?>
+			<nav>
+				<ul class="menu">
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="index.php?controlador=Usuario&operacion=registro">Registro</a></li>
+				</ul>
+				<?php 
+				//pone el menú del administrador
+				if($usuario && $usuario->admin){	?>
+				<ul class="menu">
+					<li><a href="#">ADMIN</a></li>
+				</ul>
+							
+				<?php }	?>
+			</nav>
+		<?php }
+		
+		//PONE EL PIE DE PAGINA
+		public static function footer(){	?>
+			<footer>
+				<p>  
+					<a href="index.php">Pre-Inscripcions - CIFO del Vallès</a>. 
+         		</p>
+			</footer>
+		<?php }
+	}
+?>
---
+++ preinscripcions/view/welcome_message.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<base href="<?php echo Config::get()->url_base;?>" />
+		<meta charset="UTF-8">
+		<title>Preinscripcions</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo Config::get()->css;?>" />
+		<link rel="stylesheet" type="text/css" href="<?php echo Config::get()->css2;?>" />
+	</head>
+	
+	<body>
+		<?php 
+			Template::header(); //pone el header
+
+			if(!$usuario) Template::login(); //pone el formulario de login
+			else Template::logout($usuario); //pone el formulario de logout
+			
+			Template::menu($usuario); //pone el menú
+		?>
+
+		<section id="content">
+			<h2>Pre-Inscripcions</h2>
+			<p>Apuntate a los cursos que te interese</p>
+			
+			<p>Es un ejemplo de arquitectura modelo-vista-controlador sencillo para entender los
+			conceptos y poder trabajar con él.</p>
+			
+			<p>A lo largo del curso se desarrollarán varios proyectos de ejemplo usando este pequeño framework,
+			para ir entendiendo los conceptos básicos comunes a este tipo de herramientas de trabajo MVC existentes en PHP.</p>
+			
+			<p>NO ES 100% SEGURO, así que no se debe usar para desarrollos en entornos de producción.</p>
+			
+			<p>En el mismo curso, en el último módulo, utilizaremos otro CodeIgniter para desarrollos más complejos.</p>
+		</section>
+		
+		<?php Template::footer();?>
+    </body> 
+</html>
